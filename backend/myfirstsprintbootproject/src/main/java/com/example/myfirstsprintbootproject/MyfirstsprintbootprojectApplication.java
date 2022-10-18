206f14dce895f8fcfca80d7b2fcbf61223ee2c69
Brandon <branyzp@gmail.com>
2022-10-19 02:24:04 +0800

    first commit, lfg

---
+++ backend/myfirstsprintbootproject/src/main/java/com/example/myfirstsprintbootproject/MyfirstsprintbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirstsprintbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstsprintbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstsprintbootprojectApplication.class, args);
+	}
+
+}
---
+++ backend/myfirstsprintbootproject/src/test/java/com/example/myfirstsprintbootproject/MyfirstsprintbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfirstsprintbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstsprintbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
